cae6f882edfa8f3f87211cf14e515648ff4d4fc1
lepatrick714 <leduylamc@gmail.com>
2016-05-19 01:33:56 -0700

    Pushing 1st working color ID

---
+++ ColorID/CMakeFiles/colorID.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/colorID.dir/colorID.cpp.o"
+  "colorID.pdb"
+  "colorID"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/colorID.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
